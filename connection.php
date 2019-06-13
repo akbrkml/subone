78adceebafd247efa0f8c8e472bb59acb746d3bc
Kamaludin Akbar <akbarmal80@gmail.com>
2019-06-13 18:57:27 +0700

    fix connection

--- connection.php
+++ connection.php
@@ -1,11 +1,8 @@
 <?php
-    $serverName = "sub-one.database.windows.net";
-    $connectionOptions = array(
-        "Database" => "subdb", 
-        "UID" => "badrun",
-        "PWD" => "Adele213",
-        "CharacterSet" => "UTF-8"
-    );
+    $connectionInfo = array("UID" => "badrun@sub-one", "pwd" => "Adele213", "Database" => "subdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:sub-one.database.windows.net,1433";
+    
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if ($conn) {
