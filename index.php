f356facc1c1a54ff5c8505afa4388cea12154168
Kamaludin Akbar <akbarmal80@gmail.com>
2019-06-13 20:54:33 +0700

    fix connection

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ include "connection.php";
 include "form.php";
 
 echo "
-<center><table border=1>
+<table border=1>
 <tr>
     <td><b><center>No</td>
-    <td><b><center>Full Name</td>
-    <td><b><center>Job</td>
-    <td><b><center>Date</td>
+    <td width=400><b><center>Full Name</td>
+    <td width=300><b><center>Job</td>
+    <td width=100><b><center>Date</td>
 </tr>";
 
     $sql_select = "SELECT * FROM [dbo].[Persons]";
