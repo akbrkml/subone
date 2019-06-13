a86bea29cd056c411526f1da0612357b1294be26
Kamaludin Akbar <akbarmal80@gmail.com>
2019-06-13 20:55:38 +0700

    fix connection

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ include "form.php";
 echo "
 <table border=1>
 <tr>
-    <td><b><center>No</td>
-    <td width=400><b><center>Full Name</td>
-    <td width=300><b><center>Job</td>
-    <td width=100><b><center>Date</td>
+    <td><b>No</td>
+    <td width=400><b>Full Name</td>
+    <td width=300><b>Job</td>
+    <td width=100><b>Date</td>
 </tr>";
 
     $sql_select = "SELECT * FROM [dbo].[Persons]";
